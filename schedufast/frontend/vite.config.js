92dc1afce99daa12b79e6ef9bd3985bb82772c5d
ST10490335 Aaron Ellman <ST10490335@vcconnect.edu.za>
2025-09-17 15:59:20 +0200

    Added rollupOptions to viteconfig for index and signup page

--- schedufast/frontend/vite.config.js
+++ schedufast/frontend/vite.config.js
@@ -2,4 +2,12 @@ import { defineConfig } from 'vite';
 
 export default defineConfig({
   root: 'src',
+  build: {
+    rollupOptions: {
+      input: {
+        main: 'src/index.html',
+        signup: 'src/signup.html',
+      },
+    },
+  },
 });
